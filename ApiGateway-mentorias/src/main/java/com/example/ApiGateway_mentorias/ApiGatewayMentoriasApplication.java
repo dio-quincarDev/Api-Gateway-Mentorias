b8294f2fcf23257a0f3653b34a99b72e7a0b36ce
dio-quincarDev <dio-quincar@outlook.com>
2024-11-28 11:26:52 -0500

    Apigateway Inicial

---
+++ ApiGateway-mentorias/src/main/java/com/example/ApiGateway_mentorias/ApiGatewayMentoriasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiGateway_mentorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMentoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMentoriasApplication.class, args);
+	}
+
+}
---
+++ ApiGateway-mentorias/src/test/java/com/example/ApiGateway_mentorias/ApiGatewayMentoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiGateway_mentorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMentoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
